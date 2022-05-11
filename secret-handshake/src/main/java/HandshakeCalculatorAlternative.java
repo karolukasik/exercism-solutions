d1cce9ffbb85922662c2e82a2e5803169a06da3d
karolukasik <karo.lukasik@gmail.com>
2022-05-11 20:07:49 +0200

    Alternative solution for SecretHandshake (bitwise operators)

--- secret-handshake/src/main/java/HandshakeCalculatorAlternative.java
+++ secret-handshake/src/main/java/HandshakeCalculatorAlternative.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class HandshakeCalculatorAlternative {
@@ -7,8 +8,29 @@ public class HandshakeCalculatorAlternative {
 
     List<Signal> calculateHandshake(int number) {
         List<Signal> handshakes = new ArrayList<>();
+        if ((number & 1) << 0 != 0)
+            handshakes.add(Signal.WINK);
+        if ((number & 1 << 1) != 0)
+            handshakes.add(Signal.DOUBLE_BLINK);
+        if ((number & 1 << 2) != 0)
+            handshakes.add(Signal.CLOSE_YOUR_EYES);
+        if ((number & 1 << 3) != 0)
+            handshakes.add(Signal.JUMP);
+        if ((number & 1 << 4) != 0)
+            Collections.reverse(handshakes);
 
         return handshakes;
 
     }
+
+    public static void main(String[] args) {
+        HandshakeCalculatorAlternative calc = new HandshakeCalculatorAlternative();
+        int number = 19;
+        List<Signal> secretHandshake = calc.calculateHandshake(number);
+
+        for (Signal signal : secretHandshake) {
+            System.out.println(signal);
+        }
+    }
+
 }
