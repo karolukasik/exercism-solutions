51925b1980b51f3a9edb5c7ff634f65597e59c60
karolukasik <karo.lukasik@gmail.com>
2022-05-19 20:50:54 +0200

    Improved solution for RotationalCipher (functional)

--- rotational-cipher/src/main/java/RotationalCipher.java
+++ rotational-cipher/src/main/java/RotationalCipher.java
@@ -10,27 +10,36 @@ class RotationalCipher {
     }
 
     String rotate(String data) {
+        Function<Integer, Integer> rotateLetter = createRotateLetter(this.shiftKey);
+
         return data.chars()
                 .boxed()
-                .map(createHandleStringLetter(shiftKey))
+                .map(rotateLetter)
                 .map(c -> Character.toString(c))
                 .collect(Collectors.joining());
 
     }
 
-    private static Function<Integer, Integer> createHandleStringLetter(int shiftKey) {
+    private static Function<Integer, Integer> createRotateLetter(int shiftKey) {
         return (charInASCII) -> {
             if (charInASCII >= 97 && charInASCII <= 122) {
-                return (charInASCII + shiftKey <= 122) ? charInASCII + shiftKey
-                        : 96 + (charInASCII - 122) + shiftKey;
+                return rotateSmallLetter(charInASCII, shiftKey);
             } else if (charInASCII >= 65 && charInASCII <= 90) {
-                return (charInASCII + shiftKey <= 90) ? charInASCII + shiftKey
-                        : 64 + (charInASCII - 90) + shiftKey;
+                return rotateCapitalLetter(charInASCII, shiftKey);
             } else {
                 return (charInASCII);
             }
         };
+    }
 
+    private static Integer rotateSmallLetter(Integer charInASCII, int shiftKey) {
+        return (charInASCII + shiftKey <= 122) ? charInASCII + shiftKey
+                : 96 + (charInASCII - 122) + shiftKey;
+    }
+
+    private static Integer rotateCapitalLetter(Integer charInASCII, int shiftKey) {
+        return (charInASCII + shiftKey <= 90) ? charInASCII + shiftKey
+                : 64 + (charInASCII - 90) + shiftKey;
     }
 
     public static void main(String[] args) {
