abe5150b657e3337bd390eeb9dc661f5af0f6506
karolukasik <karo.lukasik@gmail.com>
2022-07-31 19:08:27 +0200

    added alternative solution for reverse-string

--- reverse-string/src/main/java/ReverseString.java
+++ reverse-string/src/main/java/ReverseString.java
@@ -1,7 +1,17 @@
 class ReverseString {
 
-    String reverse(String inputString) {
+    
+    String reverse(String inputString){
         return new StringBuilder(inputString).reverse().toString();
     }
-  
+    
+    // static String reverse(String inputString) {
+    //     String outString = "";
+    //     for (char c : inputString.toCharArray()) {
+    //         outString = c + outString;
+    //     }
+    //     return outString;
+    // }
+
+
 }
