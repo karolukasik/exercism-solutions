abc020c769cfcf8f56b8941143282eeae291b030
karolukasik <karo.lukasik@gmail.com>
2022-08-22 18:34:54 +0200

    added corrected solution for anagram

--- anagram/src/main/java/Anagram.java
+++ anagram/src/main/java/Anagram.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,7 +8,7 @@ public class Anagram {
     private List<Integer> charsInWordSorted;
 
     public Anagram(String word) {
-        this.word = word;
+        this.word = word.toLowerCase();
         this.charsInWordSorted = convertStringToListOfIntegers(this.word);
     }
 
@@ -15,6 +16,9 @@ public class Anagram {
         List<String> matchingWords = new ArrayList<>();
 
         for (String candidate : candidates) {
+            if (candidate.toLowerCase().equals(this.word)) {
+                continue;
+            }
             List<Integer> charsInCandidateSorted = convertStringToListOfIntegers(candidate);
             if (charsInCandidateSorted.equals(this.charsInWordSorted)) {
                 matchingWords.add(candidate);
@@ -27,5 +31,4 @@ public class Anagram {
         return word.toLowerCase().chars().sorted().boxed()
                 .collect(Collectors.toCollection(ArrayList::new));
     }
-
 }
