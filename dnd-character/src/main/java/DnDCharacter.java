8aff918ed99ab8ef8f6231b1a4cfcaf6702e38e0
karolukasik <karo.lukasik@gmail.com>
2022-10-16 19:50:20 +0200

    added solution for dungeons&dragons

---
+++ dnd-character/src/main/java/DnDCharacter.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+class DnDCharacter {
+
+    private Random rnd = new Random();
+    private ArrayList<Integer> dicesRolls = new ArrayList<>();
+    private int strength;
+    private int dexterity;
+    private int constitution;
+    private int intelligence;
+    private int wisdom;
+    private int charisma;
+    private int hitpoints;
+
+    public DnDCharacter() {
+        this.strength = ability();
+        this.dexterity = ability();
+        this.constitution = ability();
+        this.intelligence = ability();
+        this.wisdom = ability();
+        this.charisma = ability();
+        this.hitpoints = 10 + modifier(this.constitution);
+    }
+
+    public static void main(String[] args) {
+        DnDCharacter viking = new DnDCharacter();
+
+    }
+
+    int ability() {
+        for (int i = 0; i < 4; i++) {
+            dicesRolls.add(rollTheDice());
+        }
+
+        dicesRolls.sort(null);
+
+        int sumOfRolls = 0;
+        for (int i = 1; i < 4; i++) {
+            sumOfRolls += dicesRolls.get(i);
+        }
+
+        return sumOfRolls;
+    }
+
+    int modifier(int input) {
+        double mod = ((double) input - 10) / 2;
+        return (int) (Math.floor(mod));
+
+    }
+
+
+
+    int getStrength() {
+        return this.strength;
+    }
+
+    int getDexterity() {
+        return this.dexterity;
+    }
+
+    int getConstitution() {
+        return this.constitution;
+    }
+
+    int getIntelligence() {
+        return this.intelligence;
+    }
+
+    int getWisdom() {
+        return this.wisdom;
+    }
+
+    int getCharisma() {
+        return this.charisma;
+    }
+
+    int getHitpoints() {
+        return this.hitpoints;
+    }
+
+    int rollTheDice() {
+        return rnd.nextInt(6) + 1;
+    }
+
+}
