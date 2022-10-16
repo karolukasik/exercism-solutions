b5dfabe79e497c1b9e771a616a5d74b3fb3a2f9b
karolukasik <karo.lukasik@gmail.com>
2022-10-16 14:52:19 +0200

    added solution for darts

---
+++ darts/src/main/java/Darts.java
@@ -0,0 +1,24 @@
+class Darts {
+    private double x;
+    private double y;
+
+    Darts(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int score() {
+   
+        double radius = Math.sqrt(x*x + y*y);
+        if (radius <= 1){
+            return 10;
+        } else if (radius <=5){
+            return 5;
+        } else if (radius <=10){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+}
